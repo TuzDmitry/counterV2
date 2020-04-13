c971e3f7306c397d46d69cdb456e71cb792049b2
TuzDmitry <dimatuz94@gmail.com>
2020-04-14 01:34:55 +0300

    installed LOCAL STORAGE T0 our Program/ counter V2 is Done

--- src/App.js
+++ src/App.js
@@ -10,12 +10,34 @@ class App extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        this.restoreState()
+    }
+
     state = {
         setMinValue: 0,
-        setMaxValue: 7,
+        setMaxValue: 5,
         memoryValue: 0,
         setIsDisabled: true,
     };
+    ///// метод, который будет брать текущий стейт и… сохранять его в localStorage
+    saveState = () => {
+        ////устанавливаем в localStorage под ключом "our-state"  наш стейт переделанный в  джейсон строку JSON.stringify(this.state)
+        localStorage.setItem("counter-state", JSON.stringify(this.state));
+    }
+
+    restoreState = () => {
+        ////объявляем наш стейт стартовый
+        let state = {...this.state}
+        //// считываем сохраненную ранее строку из localStorage
+        let stateAsString = localStorage.getItem("counter-state")
+        ////если таковая есть, то превращаем строку в объект и призваиваем стейту знаение из стораджа.
+        if (stateAsString) {
+            state = JSON.parse(stateAsString);
+        }
+////устанавливаем стейт или пустой или востановленный в стейт
+        this.setState(state)
+    }
 
     resetToZero = () => {
         let resetCounterNumb = this.state.setMinValue;
@@ -30,19 +52,19 @@ class App extends React.Component {
     }
 
     setSettings = () => {
-        this.setState({setIsDisabled: true, memoryValue: this.state.setMinValue})
+        this.setState({setIsDisabled: true, memoryValue: this.state.setMinValue}, this.saveState)
     }
 
     adjustValueMax = (value) => {
-        this.setState({setMaxValue: value, setIsDisabled: false})
+        this.setState({setMaxValue: value, setIsDisabled: false}, this.saveState)
         if ((value < 0) || (value <= this.state.setMinValue)) {
-            this.setState({setIsDisabled: true})
+            this.setState({setIsDisabled: true}, this.saveState)
         }
     }
     adjustValueMin = (value) => {
-        this.setState({setMinValue: value, setIsDisabled: false})
+        this.setState({setMinValue: value, setIsDisabled: false}, this.saveState)
         if ((value < 0) || (value >= this.state.setMaxValue)) {
-            this.setState({setIsDisabled: true})
+            this.setState({setIsDisabled: true}, this.saveState)
         }
     }
 
