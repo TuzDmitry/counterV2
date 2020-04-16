5a7576ee8986efc1a317523d2770583207a7fdf2
TuzDmitry <dimatuz94@gmail.com>
2020-04-16 14:57:53 +0300

    Updates

---
+++ precache-manifest.ec68b48b743d177d397782a8a09d0e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cd224ebe40ddcb07a389cc47027a1f",
+    "url": "/counterV2/index.html"
+  },
+  {
+    "revision": "fa516514fac3d46533a0",
+    "url": "/counterV2/static/css/main.a26f54cb.chunk.css"
+  },
+  {
+    "revision": "1f6eac8989d51b47ab3a",
+    "url": "/counterV2/static/js/2.8871baf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counterV2/static/js/2.8871baf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa516514fac3d46533a0",
+    "url": "/counterV2/static/js/main.e9e0111c.chunk.js"
+  },
+  {
+    "revision": "a7244ead44242c17ee1b",
+    "url": "/counterV2/static/js/runtime-main.619102ea.js"
+  }
+]);
